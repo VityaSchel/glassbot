987e4afbf6c69c5f24ffdffe154471f8428d1763
hloth <gitlab@hloth.dev>
2023-05-02 21:48:23 +0400

    Footer

---
+++ src/widgets/common/footer/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import styles from './styles.module.scss'
+import Image from 'next/image'
+import Banks from '@/assets/banks.webp'
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.innerFooter}>
+        <div>
+          <span>{process.env.NEXT_PUBLIC_COMPANY_NAME}</span>
+          <span>ИНН {process.env.NEXT_PUBLIC_COMPANY_INN}</span>
+          <span>ОГРНИП: {process.env.NEXT_PUBLIC_COMPANY_OGRNIP}</span>
+          <span>E-mail: {process.env.NEXT_PUBLIC_CONTACT_EMAIL}</span>
+          {/*<Link href='/'>Управление подпиской</Link>*/}
+        </div>
+        <div>
+          <Link href={`${process.env.NEXT_PUBLIC_USER_AGREEMENT}`}>Пользовательское соглашение</Link>
+          <Link href={`${process.env.NEXT_PUBLIC_DATA_POLICY}`}>Политика обработки данных</Link>
+          {/* <Link href='/subscription-cancellation'>Отмена подписки</Link> */}
+          <Link href={`${process.env.NEXT_PUBLIC_TARIFFS}`}>Тарифы</Link>
+        </div>
+      </div>
+      <div className={styles.accept}>
+        <span>Мы принимаем: </span>
+        <Image src={Banks} alt='Банки' height={32} />
+      </div>
+    </footer>
+  )
+}
